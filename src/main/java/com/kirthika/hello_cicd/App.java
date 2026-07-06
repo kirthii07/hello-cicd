fa1b01019a386fa7ba74e6d78914cf7f00891917
kirthii07 <kirthika.suku07@gmail.com>
2026-07-06 12:20:17 +0530

    Initial commit

---
+++ src/main/java/com/kirthika/hello_cicd/App.java
@@ -0,0 +1,8 @@
+package com.kirthika.hello_cicd;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello CI/CD!");
+        System.out.println("This project is built using Maven.");
+    }
+}
